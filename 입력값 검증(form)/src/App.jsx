009be63078ff45e6f8f21bdb3ecd65c60232301b
HamZik <b1y9mn@gmail.com>
2023-06-27 20:20:13 +0900

    chore: 주석 삭제

--- 입력값 검증(form)/src/App.jsx
+++ 입력값 검증(form)/src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react';
 import ValidatedInputPractice from './ValidatedInput';
 import ValidatedInputAnswer from './ValidatedInput.answer';
 
-/// input이 올바른 이메일인지 검사하는 함수, 정규 표현식 사용해서 형식 검사
 const isEmail = (input) =>
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     input
   );
-/// input이 숫자로만 이루어져 있는지 검사하는 함수, 정규 표현식 사용해서 모든 문자가 0-9 내에 들어가는지 검사
+
 const isNumeric = (input) => /^[0-9]+$/.test(input);
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
-  // 정답 확인 모드인지에 따라 실제로 사용되는 컴포넌트를 변경하는 코드, 무시해도 됨
   const ValidatedInput = isAnswerMode
     ? ValidatedInputAnswer
     : ValidatedInputPractice;
@@ -34,15 +32,9 @@ function App() {
         />
         정답 확인 모드
       </label>
-      {/* 이메일 확인.. */}
       <ValidatedInput
-        // ValidatedInput 컴포넌트에 props로 .. 3개를 넘겨주는?
-        // email은 위에서 state로 선언했음
         value={email}
-        // onInput은 선언한 email의 최신값?
         onInput={setEmail}
-        // validators는 객체임. 두 개의 원소가 있음.
-        // 근데 fn이 뭔지 모르겠음. 위에는 함수고 아래는..아래도 함수네
         validators={[
           {
             fn: (input) => input.length > 0,
@@ -54,9 +46,6 @@ function App() {
           },
         ]}
       />
-      {/* 여기까지 보고 ValidatedInput.jsx로 가보자 */}
-
-      {/* 전화번호 확인.. */}
       <ValidatedInput
         value={phoneNumber}
         onInput={setPhoneNumber}
--- 입력값 검증(form)/src/ValidatedInput.jsx
+++ 입력값 검증(form)/src/ValidatedInput.jsx
@@ -1,31 +1,13 @@
 import { useState } from 'react';
 
-// 여기 props에 props.value, props.onInput, props.validators가 들어감
 const ValidatedInput = (props) => {
-  // props.value가 null도 아니고 undefined도 아니면 props.value 외에는 ''
   const [value, setValue] = useState(props.value ?? '');
 
   const [error, setError] = useState();
 
-  // 여기를...만들면...되는 것 같음....
-  // 일단 input이 입력되게 해야.. 헐됏다
   const onChange = (e) => {
     setValue(e.target.value);
 
-    // 이제 검증을 해야..
-    // console.log(props.validators[0].fn(e.target.value));
-    // console.log(props.validators[1].fn(e.target.value));
-
-    // if (!props.validators[0].fn(e.target.value)) {
-    //   setError(props.validators[0].message);
-    // } else if (!props.validators[1].fn(e.target.value)) {
-    //   setError(props.validators[1].message);
-    // } else if (!props.validators[2].fn(e.target.value)) {
-    //   setError(props.validators[2].message);
-    // } else {
-    //   setError('');
-    //   props.onInput(e.target.value);
-    // }
     let hasError = false;
 
     props.validators.forEach((rule) => {
