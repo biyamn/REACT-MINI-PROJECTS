6a2b9fc65af9b50554bca860fdfc1cbcbdc84f63
HamZik <b1y9mn@gmail.com>
2023-06-27 20:06:15 +0900

    feat: form => React Hook Form

--- 입력값 검증(React Hook Form)/src/App.jsx
+++ 입력값 검증(React Hook Form)/src/App.jsx
@@ -1,91 +1,60 @@
-import { useState } from 'react';
-import ValidatedInputPractice from './ValidatedInput';
-import ValidatedInputAnswer from './ValidatedInput.answer';
-
-// // input이 올바른 이메일인지 검사하는 함수, 정규 표현식 사용해서 형식 검사
-// const isEmail = (input) =>
-//   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-//     input
-//   );
-// // input이 숫자로만 이루어져 있는지 검사하는 함수, 정규 표현식 사용해서 모든 문자가 0-9 내에 들어가는지 검사
-// const isNumeric = (input) => /^[0-9]+$/.test(input);
+import { useForm } from 'react-hook-form';
 
 function App() {
-  const [isAnswerMode, setIsAnswerMode] = useState(true);
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    criteriaMode: "all"
+  });
 
-  const ValidatedInput = isAnswerMode
-    ? ValidatedInputAnswer
-    : ValidatedInputPractice;
+  // 현재 입력값을 실시간으로 보여주기 위해 watch 사용
+  const mailValue = watch("email");
+  const phoneNubmberValue = watch("phoneNumber");
+  
+  // 제출 버튼을 눌렀을 때: 아무 기능 없음 
+  const onSubmit = (data) => {
+    return
+  };
 
   return (
-    <div className="flex flex-col my-4 gap-2 items-center">
-      <label className="flex gap-2">
-        <input
-          type="checkbox"
-          checked={isAnswerMode}
-          onChange={(e) => {
-            setIsAnswerMode(e.target.checked);
-            setEmail('');
-            setPhoneNumber('');
-          }}
-        />
-        정답 확인 모드
-      </label>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col my-4 gap-2 items-center">
+      <h1>[리팩토링] form => React Hook Form</h1>
       <div className="inline-block">
         <div className="flex flex-col items-center">
           <input 
-            {...register("email")}
+            {...register("email", {
+              required: "내용을 입력해주세요.",
+              pattern: {
+                value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "올바른 형식의 이메일을 입력해주세요."
+              },
+            })}
             className="border border-gray-300 rounded p-2"
           />
-          <span className="text-red text-sm font-medium">{error}</span>
+          {errors.email && <span className="text-red text-sm font-medium">{errors.email.message}</span>}
         </div>
       </div>
       <div className="inline-block">
         <div className="flex flex-col items-center">
           <input
-            {...register("phoneNumber")}
+            {...register("phoneNumber", {
+              required: "내용을 입력해주세요.",
+              minLength: { 
+                value: 10,
+                message: "10자 이상 입력해주세요."
+              },
+              pattern: {
+                value: /^[0-9]+$/,
+                message: "숫자만 입력해주세요."
+              },
+            })}
             className="border border-gray-300 rounded p-2"
           />
-          <span className="text-red text-sm font-medium">{error}</span>
+          {errors.phoneNumber && <span className="text-red text-sm font-medium">{errors.phoneNumber.message}</span>}
         </div>
       </div>
-      {/* <ValidatedInput
-        value={email}
-        onInput={setEmail}
-        validators={[
-          {
-            fn: (input) => input.length > 0,
-            message: '내용을 입력해주세요.',
-          },
-          {
-            fn: isEmail,
-            message: '올바른 형식의 이메일을 입력해주세요.',
-          },
-        ]}
-      />
-      <ValidatedInput
-        value={phoneNumber}
-        onInput={setPhoneNumber}
-        validators={[
-          {
-            fn: (input) => input.length > 0,
-            message: '내용을 입력해주세요.',
-          },
-          {
-            fn: isNumeric,
-            message: '숫자만 입력해주세요.',
-          },
-          {
-            fn: (input) => input.length >= 10,
-            message: '10자 이상 입력해주세요.',
-          },
-        ]}
-      /> */}
-      <p>이메일: {email}</p>
-      <p>전화번호: {phoneNumber}</p>
-    </div>
+      <p>이메일: {mailValue}</p>
+      <p>전화번호: {phoneNubmberValue}</p>
+      <input type="submit" value="로그인하기" className="border border-gray-300 rounded p-2" />
+    </form>
   );
 }
 
